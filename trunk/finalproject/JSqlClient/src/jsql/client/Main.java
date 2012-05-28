4b12791a1f795057d05dac1812b928d7685448c7
vinsentnguyen@gmail.com <vinsentnguyen@gmail.com@5e824919-1213-abda-2b91-2a89b2efdd1c>
2012-05-28 17:59:26 +0000

    update

--- trunk/finalproject/JSqlClient/src/jsql/client/Main.java
+++ trunk/finalproject/JSqlClient/src/jsql/client/Main.java
@@ -11,6 +11,7 @@ import java.net.UnknownHostException;
 public class Main extends JFrame implements ActionListener{
 
 	private JButton jB_Connect;
+	private JButton jB_Discon;
     private JButton jB_Execute;
     private JLabel jLabel1;
     private JPanel jP_Result;
@@ -19,97 +20,124 @@ public class Main extends JFrame implements ActionListener{
     private JScrollPane jScrollPane2;
     private JTextArea jTA_Statement;
     private JTextField jTF_IP;
+    private JTable jTable1;
 	
 	public Main(){
-		jLabel1 = new JLabel();
-        jTF_IP = new JTextField();
-        jB_Connect = new JButton();
-        jB_Execute = new JButton();
-        jP_Statement = new JPanel();
-        jScrollPane1 = new JScrollPane();
-        jScrollPane2 = new JScrollPane();
-        jTA_Statement = new JTextArea();
-        jP_Result = new JPanel();
+		jLabel1 = new javax.swing.JLabel();
+        jTF_IP = new javax.swing.JTextField();
+        jB_Connect = new javax.swing.JButton();
+        jB_Execute = new javax.swing.JButton();
+        jP_Statement = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTA_Statement = new javax.swing.JTextArea();
+        jP_Result = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jB_Discon = new javax.swing.JButton();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel1.setText("JSql Server Address:");
 
-        jTF_IP.setFont(new Font("Tahoma", 0, 14)); // NOI18N
+        jTF_IP.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jTF_IP.setText("172.0.0.1:3456");
+        jTF_IP.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
 
-        jB_Connect.setFont(new Font("Tahoma", 0, 14)); // NOI18N
+        jB_Connect.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jB_Connect.setText("Connect");
+        jB_Connect.setActionCommand("Connect");
+        jB_Connect.addActionListener(this);
 
-        jB_Execute.setFont(new Font("Tahoma", 0, 14)); // NOI18N
+        jB_Discon.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jB_Discon.setText("Disconnect");
+        jB_Discon.setActionCommand("Disconnect");
+        jB_Discon.addActionListener(this);
+
+        jB_Execute.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jB_Execute.setText("Execute");
+        jB_Execute.setActionCommand("Execute");
+        jB_Execute.addActionListener(this);
 
-        jP_Statement.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Statement"));
+        jP_Statement.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Statement"));
 
         jTA_Statement.setColumns(20);
-        jTA_Statement.setFont(new Font("Monospaced", 0, 14)); // NOI18N
+        jTA_Statement.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
         jTA_Statement.setRows(5);
         jScrollPane1.setViewportView(jTA_Statement);
 
-        GroupLayout jP_StatementLayout = new GroupLayout(jP_Statement);
+        javax.swing.GroupLayout jP_StatementLayout = new javax.swing.GroupLayout(jP_Statement);
         jP_Statement.setLayout(jP_StatementLayout);
         jP_StatementLayout.setHorizontalGroup(
-            jP_StatementLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            jP_StatementLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1)
         );
         jP_StatementLayout.setVerticalGroup(
-            jP_StatementLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            jP_StatementLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jP_StatementLayout.createSequentialGroup()
-                .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        jP_Result.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Result"));
+        jP_Result.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Result"));
 
-        javax.swing.GroupLayout jP_ResultLayout = new GroupLayout(jP_Result);
+        jTable1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Họ tên", "Tuổi"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jP_ResultLayout = new javax.swing.GroupLayout(jP_Result);
         jP_Result.setLayout(jP_ResultLayout);
         jP_ResultLayout.setHorizontalGroup(
-            jP_ResultLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            jP_ResultLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane2)
         );
         jP_ResultLayout.setVerticalGroup(
-            jP_ResultLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
+            jP_ResultLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
         );
 
-        GroupLayout layout = new GroupLayout(getContentPane());
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                    .addComponent(jP_Result, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jP_Statement, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTF_IP, GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jB_Connect, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jB_Execute, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jP_Result, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jP_Statement, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTF_IP, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jB_Connect, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jB_Discon)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jB_Execute, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTF_IP, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTF_IP, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jB_Connect)
-                    .addComponent(jB_Execute))
-                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jP_Statement, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jP_Result, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jB_Execute)
+                    .addComponent(jB_Discon))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jP_Statement, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jP_Result, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -145,26 +173,29 @@ public class Main extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-
-		switch(e.getActionCommand()){
-		case "connect":
-			if(jTF_IP.getText() == null){
-				break;
+		
+		if("Connect".equals(e.getActionCommand())){
+			if(jTF_IP.getText() != null){
+				try {
+					Socket socClient = new Socket("172.0.0.1", 3456);
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					System.out.println("UnknownHostException: " + e1.getMessage());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					System.out.println("IOException: " + e1.getMessage());
+				}
 			}
-			try {
-				Socket socClient = new Socket("172.0.0.1", 3456); // cat chuoi string
-			} catch (UnknownHostException e1) {
-				// TODO Auto-generated catch block
-				System.out.println("UnknownHostException: " + e1.getMessage());
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				System.out.println("IOException: " + e1.getMessage());
-			}
-		case "execute":
-			//add table thong bao message va error va ket qua
+		}
+		
+		if("Disconnect".equals(e.getActionCommand())){
 			
 		}
+
+		if("Execute".equals(e.getActionCommand())){
+			
+		}
+
 	}
 
 }
