21075226e8b5dda545b0d92891d02ff27a14e7d8
mr.dwater@gmail.com <mr.dwater@gmail.com@5e824919-1213-abda-2b91-2a89b2efdd1c>
2012-06-11 00:41:11 +0000

    fhghghghg

--- finalproject/JSqlServer/src/jsql/data/Database.java
+++ finalproject/JSqlServer/src/jsql/data/Database.java
@@ -118,7 +118,11 @@ public class Database implements Serializable {
 				throw new Exception("select is null!");
 			
 			select.setDatabase(this);
+<<<<<<< .mine
+			return select.executeQuery(select);		
+=======
 			return select.executeSelect(null);		
+>>>>>>> .r127
 			
 		} catch (Exception e) {
 			e.printStackTrace();
